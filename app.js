0df290b1d3f14383166e71995d6bf5937cd654a2
july1012 <113192987+july1012@users.noreply.github.com>
2023-10-17 13:15:09 -0400

    sgafdefr

--- app.js
+++ app.js
@@ -2,11 +2,12 @@ DOMSelectors = {
     form: document.querySelector("#form"),
     color: document.querySelector(".color-name"),
     h2s: document.querySelectorAll("h2"),
-
 };
 
-DOMSelectors.form.addEventLister("submit", function(event){
+console.log(DOMSelectors.form);
+
+DOMSelectors.form.addEventListener("submit", function(event){
     event.preventDefault();
     console.log(DOMSelectors.color.value);
-    DOMSelectors.h2s.forEach((header)=> header.textContent = DOMSelectors.color.value);
+//     DOMSelectors.h2s.forEach((header)=> header.textContent = DOMSelectors.color.value);
 });
